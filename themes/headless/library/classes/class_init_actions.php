e08d8698120247b747dbd0fb52b69aec1c0a9fda
Greg Nemes <mail@gregnemes.com>
2017-02-28 09:28:35 -0500

    change hero small image size

--- themes/headless/library/classes/class_init_actions.php
+++ themes/headless/library/classes/class_init_actions.php
@@ -32,7 +32,7 @@ class WS_Init_Actions extends WS_Action_Set {
 			add_image_size( 'facebook-small', 600, 315, true );	
 			add_image_size( 'facebook', 1200, 630, true );	
 			add_image_size( 'person', 500, 500, true );					
-			add_image_size( 'hero-small', 560, 183, true );
+			add_image_size( 'hero-small', 560, 151, true );
 			add_image_size( 'hero', 1680, 450, true );
 		}
 
