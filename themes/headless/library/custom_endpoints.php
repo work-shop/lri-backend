8b41d0a3f077582bf48f9968be8ead772a80925c
Nic Schumann <nicolas_schumann@brown.edu>
2017-07-30 13:39:29 -0400

    [Routes] added a form-scripts route to attempt to enqueue relevant form scripts on the Wordpress end.

--- themes/headless/library/custom_endpoints.php
+++ themes/headless/library/custom_endpoints.php
@@ -8,8 +8,20 @@ add_action( 'rest_api_init', function () {
 		) );
 });
 
+/**
+ * Handles a request of the form: `/wp-json/custom/form-scripts?form={{ int }}`,
+ * where int is given as the id of the form in question.
+ */
+add_action( 'rest_api_init', function () {
+	register_rest_route( 'custom', '/form-scripts', array(
+		'methods'   =>  'GET',
+		'callback'  =>  'get_form_scripts',
+		) );
+});
+
+
 function get_form( $request ) {
-	
+
 	$form_id = $_GET['form'];
 
 	$form = gravity_form($form_id, false, false, false, '', true, 1, false);
@@ -22,5 +34,28 @@ function get_form( $request ) {
 
 }
 
+/**
+ * Enqueues all scripts needed for WordPress, including the passed form id,
+ * Which is assumed to be an ajax enabled form.
+ */
+function get_form_scripts( $request ) {
+
+    $form_id = $_GET['form'];
+
+    if ( !empty( $form_id ) ) {
+        gravity_form_enqueue_scripts( $form_id, true );
+    }
+
+
+    ob_start();
+
+    wp_head();
+
+    $scripts = ob_get_clean();
+
+
+    return new WP_REST_Response( $scripts, true );
+
+}
 
 ?>
