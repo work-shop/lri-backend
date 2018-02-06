bfb5a23d92860f89d5400c22cfa06abbe542034c
Greg Nemes <mail@gregnemes.com>
2018-02-06 17:33:22 -0500

    changed code for gravity forms permissions

--- themes/headless/library/gravity_forms_permissions_overrides.php
+++ themes/headless/library/gravity_forms_permissions_overrides.php
@@ -15,19 +15,5 @@ function grant_gforms_editor_access() {
 // Tie into the 'after_switch_theme' hook
 add_action( 'after_switch_theme', 'grant_gforms_editor_access' );
 
-/**
- * Remove Gravity Forms capabilities from Editor role.
- * Runs when this theme is deactivated (in favor of another).
- * 
- * @access public
- * @return void
- */
-function revoke_gforms_editor_access() {
-  
-  $role = get_role( 'editor' );
-  $role->remove_cap( 'gform_full_access' );
-}
-// Tie into the 'switch_theme' hook
-add_action( 'switch_theme', 'revoke_gforms_editor_access' );
 
 ?>
