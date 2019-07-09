4ee6f33c6117ff877bafc2a0b187f0b098d356be
Greg Nemes <mail@gregnemes.com>
2019-07-09 13:07:23 -0400

    plugins and added line to remove width and height from wysiwyg images

--- themes/headless/functions.php
+++ themes/headless/functions.php
@@ -30,6 +30,13 @@ require_once( 'library/custom_endpoints.php');
 
 require_once('library/gravity_forms_permissions_overrides.php');
 
+add_filter( 'post_thumbnail_html', 'remove_width_attribute', 10 );
+add_filter( 'image_send_to_editor', 'remove_width_attribute', 10 );
+
+function remove_width_attribute( $html ) {
+	$html = preg_replace( '/(width|height)="\d*"\s/', "", $html );
+	return $html;
+}
 
 
 ?>
